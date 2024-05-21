de05043b884a48ba176c6d7d1b7e721bc1b24294
salvador <salvadorjnt@gmail.com>
2024-05-21 11:59:54 -0600

    lesson 10: async generator functions

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ import { asyncAwaitComponent } from './src/concepts/06-async-await.js'
 import { asyncAwait2Component } from './src/concepts/07-async-await.js'
 import { forAwaitComponent } from "./src/concepts/08-for-await.js";
 import { generatorFunctionsComponent } from './src/concepts/09-generator-functions.js'
+import { generatorFunctionAsyncComponent } from './src/concepts/10-generator-async.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -37,4 +38,5 @@ const element = document.querySelector('.card');
 // asyncAwaitComponent(element);
 // asyncAwait2Component(element);
 // forAwaitComponent(element);
-generatorFunctionsComponent(element);
+// generatorFunctionsComponent(element);
+generatorFunctionAsyncComponent(element);
